fd3e0537c6350dbcf3da655cc8cf570ac138bf5e
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2023-12-03 00:12:18 -0500

    Added update on interval

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './styles.css';
 
 const StockTracker = () => {
-  const [symbol, setSymbol] = useState('AAPL'); // Initial stock symbol (Apple Inc.)
+  const [symbol, setSymbol] = useState('AAPL');
   const [stockValue, setStockValue] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(60 * 60); // Initial time left in seconds (1 hour)
 
   const stockList = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA'];
 
@@ -23,6 +24,7 @@ const StockTracker = () => {
       const data = await response.json();
       const stockInfo = data.quoteResponse.result[0];
       setStockValue(stockInfo.regularMarketPrice);
+      setTimeLeft(60 * 60); // Reset the timer after each successful update
     } catch (error) {
       console.error('Error fetching stock data:', error);
     }
@@ -32,18 +34,35 @@ const StockTracker = () => {
     // Fetch initial stock value
     fetchStockValue();
 
-    // Set interval to update stock value every 5 seconds (adjust as needed)
-    const intervalId = setInterval(fetchStockValue, 5000);
+    // Set interval to update stock value every hour
+    const intervalId = setInterval(() => {
+      fetchStockValue();
+    }, 60 * 60 * 1000);
 
-    // Clean up interval on component unmount
-    return () => clearInterval(intervalId);
+    // Set interval to update timer every second
+    const timerIntervalId = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => (prevTimeLeft > 0 ? prevTimeLeft - 1 : 0));
+    }, 1000);
+
+    // Clean up intervals on component unmount
+    return () => {
+      clearInterval(intervalId);
+      clearInterval(timerIntervalId);
+    };
   }, [symbol]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Stock Tracker</h1>
       <p>Stock Symbol: {symbol}</p>
       <p>Stock Value: {stockValue !== null ? `$${stockValue.toFixed(2)}` : 'Loading...'}</p>
+      <p>Time left until the next update: {formatTime(timeLeft)}</p>
       <div>
         <p>Select a Stock Symbol:</p>
         {stockList.map((stock) => (
