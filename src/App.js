36ff70bdc024c0c854049cdcbecee74af51d01cc
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2023-12-03 00:01:28 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles.css';
 
-function App() {
+const StockTracker = () => {
+  const [symbol, setSymbol] = useState('AAPL'); // Initial stock symbol (Apple Inc.)
+  const [stockValue, setStockValue] = useState(null);
+
+  const stockList = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA'];
+
+  const fetchStockValue = async () => {
+    try {
+      const response = await fetch(
+        `https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=${symbol}`,
+        {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+          },
+        }
+      );
+
+      const data = await response.json();
+      const stockInfo = data.quoteResponse.result[0];
+      setStockValue(stockInfo.regularMarketPrice);
+    } catch (error) {
+      console.error('Error fetching stock data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch initial stock value
+    fetchStockValue();
+
+    // Set interval to update stock value every 5 seconds (adjust as needed)
+    const intervalId = setInterval(fetchStockValue, 5000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [symbol]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Stock Tracker</h1>
+      <p>Stock Symbol: {symbol}</p>
+      <p>Stock Value: {stockValue !== null ? `$${stockValue.toFixed(2)}` : 'Loading...'}</p>
+      <div>
+        <p>Select a Stock Symbol:</p>
+        {stockList.map((stock) => (
+          <button key={stock} onClick={() => setSymbol(stock)}>
+            {stock}
+          </button>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default StockTracker;
